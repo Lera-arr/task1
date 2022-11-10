89c7736810c6b5eee74a50e0d64fdcacedef88ea
Lera-arr <lerok-mazurenko@yandex.ru>
2022-11-10 17:50:40 +0300

    1

--- B1/BMW/Program.cs
+++ B1/BMW/Program.cs
@@ -11,6 +11,7 @@ namespace BMW
         static void Main(string[] args)
         {
             Console.WriteLine("ЁЛКИ ПАЛКИ!");
+            vgcfrcf
             Console.ReadKey();
         }
     }
